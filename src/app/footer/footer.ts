089b8f1c56abb8683ab81af2db9931e6026a2e1c
Mahmoud Diallo <mahmouddiallo625@gmail.com>
2026-09-04 15:23:56 +0000

    Suppression du code html brute dans app.html

--- src/app/footer/footer.ts
+++ src/app/footer/footer.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -6,4 +6,10 @@ import { Component } from '@angular/core';
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
-export class Footer {}
+export class Footer {
+  annee = signal(2026);
+
+  changerAnnee() {
+    this.annee.update((valeur) => valeur + 1);
+  }
+}
