d509a16a7a4c243d7cd9c3d3a0d59638b9cb8af7
Mahmoud Diallo <mahmouddiallo625@gmail.com>
2026-09-04 18:07:21 +0000

    Ajout de la navigation barre et création du signal toggleMenu dans le composant header

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Footer } from './footer/footer';
+import { Header } from './header/header';
 @Component({
   selector: 'app-root',
-  imports: [
-    RouterOutlet,
-    Footer
-
-  ],
+  imports: [RouterOutlet, Footer, Header],
   standalone: true,
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('mon-premier-projet');
--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,4 +6,12 @@ import { Component } from '@angular/core';
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {}
+export class Header {
+  // signal: une donnée réactive pour le meu mobile
+  isMenuOpen = signal(false);
+
+  // methode : pour basculer l'état du menu
+  toggleMenu() {
+    this.isMenuOpen.update((etatActuel) => !etatActuel);
+  }
+}
