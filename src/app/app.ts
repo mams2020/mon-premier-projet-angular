c989237e681578cc03322c3f54c18189d3aeeabe
Mahmoud Diallo <mahmouddiallo625@gmail.com>
2026-09-04 22:46:52 +0000

    feat: Ajout de contenu pour le component hero

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,9 +2,10 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Footer } from './footer/footer';
 import { Header } from './header/header';
+import { Hero } from './hero/hero';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Footer, Header],
+  imports: [RouterOutlet, Footer, Header, Hero],
   standalone: true,
   templateUrl: './app.html',
   styleUrl: './app.css',
--- src/app/skills/skills.ts
+++ src/app/skills/skills.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
 
+interface Skill {
+  nom: string;
+  categorie: string;
+}
+
 @Component({
   selector: 'app-skills',
   imports: [],
   templateUrl: './skills.html',
   styleUrl: './skills.css',
 })
-export class Skills {}
+export class Skills {
+  // simple tableau de données
+  skills: Skill[] = [
+    { nom: 'HTML / CSS / JS', categorie: 'Développement' },
+    { nom: 'Angular', categorie: 'Développement' },
+    { nom: 'React', categorie: 'Développement' },
+    { nom: 'TypeScript', categorie: 'Développement' },
+    { nom: 'Python', categorie: 'Développement' },
+    { nom: 'Figma', categorie: 'Design' },
+    { nom: 'Illustrator / Photoshop', categorie: 'Design' },
+    { nom: 'Affinity', categorie: 'Design' },
+  ];
+}
