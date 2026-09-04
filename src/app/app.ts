3bc67472d4d170a594f330097825715f62b609c5
Mahmoud Diallo <mahmouddiallo625@gmail.com>
2026-09-04 23:19:26 +0000

    feat(skills): amélioration du composant Skills avec @for et optimisation du design

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,9 +3,10 @@ import { RouterOutlet } from '@angular/router';
 import { Footer } from './footer/footer';
 import { Header } from './header/header';
 import { Hero } from './hero/hero';
+import { Skills } from './skills/skills';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Footer, Header, Hero],
+  imports: [RouterOutlet, Footer, Header, Hero, Skills],
   standalone: true,
   templateUrl: './app.html',
   styleUrl: './app.css',
